5380cce34391677b05862585391ca4a9b2b6986a
salem676 <echoesofeternal@gmail.com>
2022-07-22 13:12:19 -0500

    venga

--- 0x01-math_sequence/1-fibonacci.c
+++ 0x01-math_sequence/1-fibonacci.c
@@ -2,7 +2,7 @@
 /**
  * Fibonnaci - function return a pointer with the head of
  * the fibonacci linked list.
- * Description: as stated.
+ * Description: as above.
  * Return: Return the head of the fibonacci linked list.
  */
 t_cell *Fibonnaci()
@@ -10,11 +10,7 @@ t_cell *Fibonnaci()
 	t_cell *new, *head;
 	float x1 = 0, x2 = 1, n1 = 0, n2 = 1;
 	/*
-	 * @new: pointer to t_cell instance.
-	 * @head: pointer to t_cell instance
-	 * representing the head of structure.
-	 * xis and nis represent variables for
-	 * fibonacci calculation.
+	 *
 	 */
 	head = NULL;
 	while (x2 != x1)
@@ -24,9 +20,9 @@ t_cell *Fibonnaci()
 		{
 			return (NULL);
 		}
-			new->elt = n2;
-			new->next = head;
-			head = new;
+		new->elt = n2;
+		new->next = head;
+		head = new;
 		if (n1 != 0)
 		{
 			x1 = n1 / (n2 - n1);
@@ -39,19 +35,21 @@ t_cell *Fibonnaci()
 }
 /**
  * gold_number - Function that Return the Golden Ratio
- * of the fibonacci serie.
+ * of the fibonacci series.
  * @head: Parameter pointer to the head of the fibonacci
  * linked list.
- * Description: as above stated.
- * Return: Return the Golden Ratio of the fibonacci series.
+ * Description: as above.
+ * Return: Return the Golden Ratio of the fibonacci serie.
  */
 double gold_number(t_cell *head)
 {
 	t_cell *tmp;
 	double gr, gr1, gr2;
 	/*
-	 * @tmp: temporal t_cell instance var.
-	 * @gr: golden ratio var for calculation.
+	 * @tmp: ptr to t_cell.
+	 * @gr: double var.
+	 * @gr1: double var.
+	 * @gr2: double var.
 	 */
 	tmp = head;
 	gr1 = tmp->elt;
